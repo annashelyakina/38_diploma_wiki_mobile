06e4535b2490e325b2bab4dbfdc6e46c47ec0d9a
annashelyakina <anna.shelyakina82@gmail.com>
2026-01-20 20:42:57 +0300

    Updated SearchScreen

--- src/test/java/screens/SearchScreen.java
+++ src/test/java/screens/SearchScreen.java
@@ -1,5 +1,7 @@
 package screens;
 
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 import tests.Constants;
 
@@ -11,42 +13,51 @@ import static io.appium.java_client.AppiumBy.id;
 
 public class SearchScreen {
 
+    private final SelenideElement
+        textInSearchFieldEng = $(accessibilityId(Constants.ENG_TEXT_IN_SEARCH_FIELD)),
+        textInSearchFieldRu = $(accessibilityId(Constants.RU_TEXT_IN_SEARCH_FIELD)),
+        searchField = $(id("org.wikipedia.alpha:id/search_src_text"))
+    ;
+
+    private final ElementsCollection
+            searchedValuesList =$$(id("org.wikipedia.alpha:id/page_list_item_title"))
+    ;
+
     @Step("Type and search 'Appium' in English Wikipedia")
     public SearchScreen typeAndSearchAppiumEng(){
-        $(accessibilityId(Constants.ENG_TEXT_IN_SEARCH_FIELD)).click();
-        $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys(Constants.APPIUM);
+        textInSearchFieldEng.click();
+        searchField.sendKeys(Constants.APPIUM);
 
         return this;
     }
 
     @Step("Type and search 'Java' in English Wikipedia")
     public SearchScreen typeAndSearchJavaEng(){
-        $(accessibilityId(Constants.ENG_TEXT_IN_SEARCH_FIELD)).click();
-        $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys(Constants.JAVA);
+        textInSearchFieldEng.click();
+        searchField.sendKeys(Constants.JAVA);
 
         return this;
     }
 
     @Step("Type and search 'Appium' in Russian Wikipedia")
     public SearchScreen typeAndSearchAppiumRu(){
-        $(accessibilityId(Constants.RU_TEXT_IN_SEARCH_FIELD)).click();
-        $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys(Constants.APPIUM);
+        textInSearchFieldRu.click();
+        searchField.sendKeys(Constants.APPIUM);
 
         return this;
     }
 
     @Step("Type and search 'Java' in Russian Wikipedia")
     public SearchScreen typeAndSearchJavaRu(){
-        $(accessibilityId(Constants.RU_TEXT_IN_SEARCH_FIELD)).click();
-        $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys(Constants.JAVA);
+        textInSearchFieldRu.click();
+        searchField.sendKeys(Constants.JAVA);
 
         return this;
     }
 
     @Step("Verify found content")
     public SearchScreen verifyFoundContent(){
-        $$(id("org.wikipedia.alpha:id/page_list_item_title"))
-                .shouldHave(sizeGreaterThan(0));
+        searchedValuesList.shouldHave(sizeGreaterThan(0));
 
         return this;
     }
